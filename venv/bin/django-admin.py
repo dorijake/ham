fec3a58cdfef484ac1d7f84a8fe9164979794e60
cybaj <cybaj@naver.com>
2017-05-23 17:30:41 +0900

    환경수정: 가상환경 제거@170523

--- venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/jake/dev/work/ham/venv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
--- wvenv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!d:\makingcompany\0_jickgong\app_dev\dev_alpha\ham\wvenv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
