cd20764839cc97d52776ce39d588b941ff48b291
cybaj <cybaj@naver.com>
2017-05-23 15:25:19 +0900

    170523 neo4jdriver installed at only windows.

--- winvenv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!d:\makingcompany\0_직공\어플리케이션개발\dev_alpha\ham\winvenv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
---
+++ wvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!d:\makingcompany\0_jickgong\app_dev\dev_alpha\ham\wvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
