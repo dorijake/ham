f99df43b4e24cb2e820ecef7a306ae74613162a3
cybaj <cybaj@naver.com>
2017-05-28 14:09:49 +0900

    @170528 neo4j 인터페이스, 초기화면 select 추가. json으로

--- src/neo/try_2.py
+++ src/neo/try_2.py
@@ -1,4 +1,5 @@
 from neo4j.v1 import GraphDatabase
+import json
 
 driver = GraphDatabase.driver("bolt://203.246.82.121:7687", auth=("neo4j", "900416"))
 
@@ -92,9 +93,20 @@ def deleteNode(session, pgsi, p_key):
   print(result) 
 
 
+# p-key로 할 수 있는 게 아니다
+# 
+# def deleteRlt(session, rlt):
+#   delete_query = "MATCH ()-[r:"+rlt+"]->() WHERE r.p_key = {p_key} DELETE r"
+#   result = session.run(delete_query, {"rlt":str(rlt), "p_key":str(p_key)})
+
+#   print(result) 
+
+
 
 def createRlt_p2p(session, p1_p_key, p2_p_key, with_num):
   create_query = "MATCH (n:Person) WHERE n.p_key = {p1_p_key} MATCH (m:Person) WHERE m.p_key = {p2_p_key} CREATE (n)-[r:On_rlt_p2p{with_num:{with_num}, last_modified:timestamp()}]->(m)"
+  result = session.run(create_query, {"p1_p_key":str(p1_p_key), "p2_p_key":str(p2_p_key), "with_num":str(with_num)})
+  create_query = "MATCH (n:Person) WHERE n.p_key = {p2_p_key} MATCH (m:Person) WHERE m.p_key = {p1_p_key} CREATE (n)-[r:On_rlt_p2p{with_num:{with_num}, last_modified:timestamp()}]->(m)"
   result = session.run(create_query, {"p1_p_key":str(p1_p_key), "p2_p_key":str(p2_p_key), "with_num":str(with_num)})
   print(result)
   return
@@ -166,29 +178,137 @@ def createRlt_p2i(session, p_p_key, i_p_key, get_num):
 #   return 
 
 
+def select_p_of_p(session, p_key):
+  # MATCH (n:Person)-[:On_rlt_p2p]->(m) WHERE n.p_key = "1" RETURN n, m
+  # create_query = "MATCH (n:Person)-[:On_rlt_p2p]->(m) WHERE n.p_key = {p_key} RETURN n.name AS a_name, m.name AS b_name"
+  create_query = "MATCH (n:Person)-[:On_rlt_p2p]->(m) WHERE n.p_key = {p_key} RETURN n, m"
+  result = session.run(create_query, {"p_key":str(p_key)})
+  r_dic = []
+  p_dic = {}
+  v_dic = {}
+  for record in result:
+    for k, v in record.items():
+      
+      if str(k) == "m" :
+        for k_1, v_1 in v.items():
+          v_dic[str(k_1)] = str(v_1)
+
+        # print(v_dic)
+        r_dic.append(v_dic)
+        print(r_dic)
+      #   r_dic.append(p_dic)
+      #   p_dic.clear()
+      # print(k)
+
+  # print(len(r_dic))
+  # print(r_dic)
+    # print(list(record['m'].keys()))
+    # print(list(record['m'].values()))
+      # print("%s %s" % (record["a_name"], record["b_name"]))
+      # print("%s %s" % (record["name"], record["incentiveStrg_no"]))
+
+# def print_friends_of(session, p_key):
+#         with session.begin_transaction() as tx:
+#             for record in tx.run("MATCH (a:Person)-[:On_rlt_p2p]->(f) "
+#                                  "WHERE a.p_key = {p_key} "
+#                                  "RETURN f.name", p_key=p_key):
+#                 print(record["f.name"])
+
+
+
+def select_node_p2s_got(session, p_key):
+  # MATCH (n:Person)-[:On_rlt_p2p]->(m) WHERE n.p_key = "1" RETURN n, m
+  # create_query = "MATCH (n:Person)-[:On_rlt_p2p]->(m) WHERE n.p_key = {p_key} RETURN n.name AS a_name, m.name AS b_name"
+  create_query = "MATCH (n:Person)-[r:On_rlt_p2s]->(m) WHERE n.p_key = {p_key} RETURN m, r.got_in_num" 
+  result = session.run(create_query, {"p_key":str(p_key)})
+  got_list = []
+  
+  for record in result:
+    # m temp_dic
+    temp_dic = dict(zip(record.values()[0].keys(), record.values()[0].values()))
+    # r.got_in_num : str
+    temp_dic["r.got_in_num"] = int(record.values()[1])
+    got_list.append(temp_dic)
+
+  # list sorting by value at dicionaries in list
+  sorted_list = sorted(got_list, key = lambda k:k["r.got_in_num"], reverse = True)
+
+  return sorted_list
+
+def encode2json_d3_first(sorted_list):
+  name_list_sorted_gotInNum = []
+
+  for store in sorted_list:
+    name_list_sorted_gotInNum.append(store["name"])
+  
+  temp_dic = {}
+  temp_dic["nodes"] = name_list_sorted_gotInNum
+  print(json.dumps(temp_dic,ensure_ascii=False))
+
+
+
+def select_node_p2s_wish(session, p_key):
+  create_query = "MATCH (n:Person)-[r:On_rlt_p2s]->(m) WHERE n.p_key = {p_key}, r.wish_on = \"1\" ORDER BY = r.got_in_num DESC RETURN m"
+  result = session.run(create_query, {"p_key":str(p_key)})
+  r_dic = []
+  p_dic = {}
+  v_dic = {}
+  for record in result:
+    print(result)
+
+def service_00_p2s():
+  print("a")
+
+
+
 session = startSession()
 
 #createNode_person(session, name, age, sex, belongto, located_in, net_netser_click_num, p_key):
-createNode_person(session, "김민석", "28", "1", "서울과학기술대학교", "1", "10", "1")
-createNode_person(session, "박종균", "27", "1", "서울과학기술대학교", "0", "10", "2")
-createNode_person(session, "민연홍", "25", "1", "서울과학기술대학교", "1", "10", "3")
-createNode_person(session, "조우영", "21", "1", "서울과학기술대학교", "1", "10", "4")
-createNode_person(session, "한동기", "24", "1", "서울과학기술대학교", "0", "10", "5")
-createNode_person(session, "박제익", "22", "1", "서울과학기술대학교", "0", "10", "6")
-createNode_person(session, "박준형", "22", "1", "서울과학기술대학교", "0", "10", "7")
+# createNode_person(session, "김민석", "28", "1", "서울과학기술대학교", "1", "10", "1")
+# createNode_person(session, "박종균", "27", "1", "서울과학기술대학교", "0", "10", "2")
+# createNode_person(session, "민연홍", "25", "1", "서울과학기술대학교", "1", "10", "3")
+# createNode_person(session, "조우영", "21", "1", "서울과학기술대학교", "1", "10", "4")
+# createNode_person(session, "한동기", "24", "1", "서울과학기술대학교", "0", "10", "5")
+# createNode_person(session, "박제익", "22", "1", "서울과학기술대학교", "0", "10", "6")
+# createNode_person(session, "박준형", "22", "1", "서울과학기술대학교", "0", "10", "7")
+# createNode_person(session, "문재인", "65", "1", "청와대", "0", "10", "8")
 
-createNode_item(session, "부대찌개", "6000", "001", "3", "2", "00000010")
-createNode_store(session, "밥은", "0001", "0001", "공릉동", "노원구", "서울시", "00000007")
+# createNode_item(session, "부대찌개", "6000", "001", "3", "2", "00000010")
+# 
+# def createNode_store(session, name, service_num, service_no, loc_dong_no, loc_gu_no, loc_si_no, p_key):
+# createNode_store(session, "밥은", "0001", "0001", "공릉동", "노원구", "서울시", "00000007")
+# createNode_store(session, "소문날라", "0001", "0001", "공릉동", "노원구", "서울시", "00000008")
+# createNode_store(session, "호야네", "0002", "0001", "공릉동", "노원구", "서울시", "00000009")
+# createNode_store(session, "호치킨", "0000", "0001", "공릉동", "노원구", "서울시", "00000010")
+# createNode_store(session, "맥도날드", "0003", "0001", "공릉동", "노원구", "서울시", "00000011")
+# createNode_store(session, "신서방 족발", "0000", "0001", "공릉동", "노원구", "서울시", "00000012")
+# createNode_store(session, "Hippo Coffe", "0001", "0001", "공릉동", "노원구", "서울시", "00000013")
 
-#createNode_group(session, name, group_type_no, p_key):
-createNode_group(session, "캡사이숑", "8", "11111111" )
+# #createNode_group(session, name, group_type_no, p_key):
+# createNode_group(session, "캡사이숑", "8", "11111111" )
+# createNode_group(session, "직공팀", "8", "22222222" )
 
-# deleteNode(session, "Group", "11111111")
+# # deleteNode(session, "Person", "8")
+# deleteRlt(session, rlt, p_key):
 
-# createRlt_p2p(session, "5", "6", "3")
+
+# createRlt_p2p(session, "1", "2", "3")
+# createRlt_p2p(session, "1", "8", "3")
+# createRlt_p2p(session, "1", "3", "3")
+# createRlt_p2p(session, "2", "3", "3")
 # createRlt_p2g(session, "5", "11111111", "3")
 # createRlt_p2s(session, "5", "00000007", "1", "0")
-createRlt_p2i(session, "5", "00000010", "3")
-createRlt_p2i(session, "6", "00000010", "3")
+# createRlt_p2s(session, "5", "00000008", "0", "5")
+# createRlt_p2s(session, "5", "00000009", "0", "6")
+# createRlt_p2s(session, "5", "00000010", "1", "0")
+# createRlt_p2s(session, "5", "00000011", "0", "11")
+# createRlt_p2s(session, "5", "00000012", "1", "0")
+# createRlt_p2s(session, "5", "00000013", "0", "10")
+# createRlt_p2i(session, "5", "00000010", "3")
+# createRlt_p2i(session, "6", "00000010", "3")
+
+
+encode2json_d3_first(select_node_p2s_got(session, "5"))
+# print_friends_of(session, "2")
 
 session.close()
