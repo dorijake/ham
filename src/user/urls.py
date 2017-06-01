6fb41417db4ca77df80802465b9b17f3f5e8404c
Jake <dorijake@gmail.com>
2017-06-01 18:19:51 +0900

    대략적인 친구추가 기능 완성
    jquery.cookie.js 추가

--- src/user/urls.py
+++ src/user/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
 	url(r'friends/$', views.friend_list, name='friends'),
 	url(r'friends/search/', views.search_user, name='search_user'),
+	url(r'friends/add/', views.add_friend, name='add_friend'),
+	url(r'friends/delete/', views.delete_friend, name='delete_friend'),
 ]
--- src/user/views.py
+++ src/user/views.py
@@ -1,17 +1,19 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from .models import User, Friends
+from .models import User, Friends, Friends_Status
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_protect
 from django.db.models import Q
 import json
-
+from .forms import FriendsForm
+from django.forms.models import model_to_dict
 # Create your views here.
 
 @login_required
 def friend_list(request):
 	current_user_id = request.user.id
 	
+
 	friend_list = User.objects.get_friends(id=current_user_id)
 	
 	
@@ -19,12 +21,11 @@ def friend_list(request):
 
 
 @login_required
-@csrf_protect
 def search_user(request):
 
 	USER_RELELATION = {'MYSELF':1, 'FRIEND':2, 'NO_RELATION':3,}
 
-	username = request.POST.get('username')
+	username = request.POST.get('keyword')
 	user_list = User.objects.filter(username__contains=username).values('id', 'username',)
 
 	current_user_id = request.user.id
@@ -53,9 +54,53 @@ def search_user(request):
 @csrf_protect
 def add_friend(request):
 
-	FRIEND_STATUS = ''
-	user_id = request.POST.get('user_id')
-	friend_id = request.POST.get('friend_id')
+	user1_id = int(request.POST.get('user1_id'))
+	user2_id = int(request.POST.get('user2_id'))
+	action_user_id = int(request.POST.get('action_user'))
+	status_id = Friends_Status.objects.get(name='requesting').code
+
+	form_data = {
+		'user1':user1_id,
+		'user2':user2_id,
+		'action_user':action_user_id,
+		'status':status_id
+	}
+
+	friends_form = FriendsForm(data=form_data)
 	
+	if request.method == "POST":
+		if friends_form.is_valid():
+			obj = friends_form.save()
+			
+			return HttpResponse(json.dumps({'friends':model_to_dict(obj)}), content_type='application/json')
+
+	return HttpResponse()
+
+
+
+def delete_friend(request):
+
+	current_user_id = request.user.id
+	friend_id = int(request.POST.get('friend_id'))
+
+	friend_list = User.objects.get_friends(current_user_id)
+	friend_id_list = [friend_id['friend_id'] for friend_id in friend_list]
+
+	friend = Friends.objects.filter(Q(user1=current_user_id, user2=friend_id) | Q(user1=friend_id, user2=current_user_id))
+
+	response = {}
+
+	if request.method == "POST":
+
+		if friend:
+			friend.delete()
+			response['result'] = 'success'
+		else:
+			response['result'] = 'failed'
+
+
+	
+	return HttpResponse(json.dumps(response), content_type='application/json')
+
 
 	
